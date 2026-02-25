4f72338db76b6109dcb00d117faf2d5456ce1885
muhsener-be <muhammet.sener@bilgiedu.net>
2026-02-25 07:13:05 +0300

    feat: Add new constructor to NotFoundException.

--- commons-core/src/main/java/github/muhsenerdev/commons/core/exception/NotFoundException.java
+++ commons-core/src/main/java/github/muhsenerdev/commons/core/exception/NotFoundException.java
@@ -8,4 +8,9 @@ public class NotFoundException extends ApplicationException {
     public NotFoundException(String code, String message) {
         super(code, message);
     }
+
+    public NotFoundException(String entity, String field, Object value) {
+        super(entity.toLowerCase() + ".not_found",
+                String.format("%s not found with %s: [%s]", entity, field, value));
+    }
 }
